35de1d8358f4a842f4b2361e8e4056e649848245
Pranoy Dev <devpranoy@gmail.com>
2018-08-17 13:08:24 +0530

    sort by district

--- app.py
+++ app.py
@@ -11,6 +11,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST']) #landing page
 def home():
+    if request.method == 'POST':
+        keyword1 = request.form['data']
+        url = 'https://byw1s98hik.execute-api.ap-south-1.amazonaws.com/dev/webapp/district'
+        data = {'keyword':keyword1}
+        headers = {'content-type': 'application/json'}
+        r=requests.post(url, data=json.dumps(data), headers=headers)
+        data = r.json()
+        request_no = data['Count']
+        return render_template("index.html",requestnumber = request_no, data = data)
+
+        
     r = requests.get('https://byw1s98hik.execute-api.ap-south-1.amazonaws.com/dev/androidapp/get')
     data = r.json()
     
