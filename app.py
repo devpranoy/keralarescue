92ce21c872a6949c005a0ca753db7730ae3e77d0
Pranoy Dev <devpranoy@gmail.com>
2018-08-18 21:06:22 +0530

    Moderator comment

--- app.py
+++ app.py
@@ -67,6 +67,49 @@ def date_processor():
        
         return datetime.fromtimestamp(ts).strftime('%Y-%m-%d %I:%M %p')
     return dict(change_epoch=change_epoch)
+
+@app.route('/moderator', methods=['GET','POST']) #landing page
+def moderator():
+    if request.method == 'POST':
+        keyword1 = request.form['data']
+        url = 'https://byw1s98hik.execute-api.ap-south-1.amazonaws.com/dev/webapp/district'
+        data = {'keyword':keyword1}
+        headers = {'content-type': 'application/json'}
+        r=requests.post(url, data=json.dumps(data), headers=headers)
+        data = r.json()
+        request_no = data['Count']
+        return render_template("moderate.html",requestnumber = request_no, data = data, district = keyword1)   
+    r = requests.get('https://byw1s98hik.execute-api.ap-south-1.amazonaws.com/dev/androidapp/get')
+    data = r.json()
+    request_no = data['Count']
+    return render_template("moderate.html",requestnumber = request_no, data = data)
+
+@app.route('/moderatorcomment/<string:timeindex>',methods=['GET','POST'])
+def remark(timeindex):
+    print(timeindex)
+    timeindexlist = timeindex.split('&')
+    if timeindexlist[1] == 'null':
+        
+        return redirect(url_for('moderator'))
+
+    url = 'https://byw1s98hik.execute-api.ap-south-1.amazonaws.com/dev/webapp/current-info-update'
+    data = {'TimeIndex':timeindexlist[0],'Current_Info':timeindexlist[1]}
+    
+    headers = {'content-type': 'application/json'}
+    r=requests.post(url, data=json.dumps(data), headers=headers)
+    data = r.json()
+    return redirect(url_for('moderator'))
+
+
+
+@app.context_processor
+def date_processor():
+    def change_epoch(epoch):
+        ts = int(epoch)
+        ts = ts/1000
+       
+        return datetime.fromtimestamp(ts).strftime('%Y-%m-%d %I:%M %p')
+    return dict(change_epoch=change_epoch)
     
 if __name__=='__main__':
 
